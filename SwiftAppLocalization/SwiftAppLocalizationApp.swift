d0bf017e2c39835ef3f3bfc0218d7895ef5f8669
Moussa Hellal <moussa@sane.agency>
2023-06-18 17:42:50 +0100

    Initial Commit

---
+++ SwiftAppLocalization/SwiftAppLocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppLocalizationApp.swift
+//  SwiftAppLocalization
+//
+//  Created by Moussa on 18/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppLocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
